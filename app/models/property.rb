7c2bff8cda7d6c1f9554d689967c99fef09171e8
Ian Harwell <sirharwell@gmail.com>
2018-04-11 10:31:45 -0600

    seeds

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,16 @@
 class Property < ApplicationRecord
   belongs_to :agent
   has_one :address
+
+  #Property.available
+  def self.available
+    select('
+              properties.id, price, beds, baths, sq_ft,
+              ad.city, ad.state, ad.street,
+              a.first_name, a.last_name, a.email, a.id AS agent_id')
+      .joins('INNER JOIN agents a ON a.id = properties.agnet_id
+              INNER JOIN addresses ad on ad.property_id = properties.id')
+      .where('properties.sold <> TRUE')
+      .order('a.id')
+    end
 end
