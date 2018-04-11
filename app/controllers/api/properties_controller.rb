dab2a0b5c965d95c63e3a4cfccbaf05a87dfd1f3
Ian Harwell <sirharwell@gmail.com>
2018-04-11 11:45:02 -0600

    pagination

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -1,6 +1,19 @@
 class Api::PropertiesController < ApplicationController
+  before_action :set_page
+
   def index
-      render json: Property.available
+    render json: Property.page(@page).available
   end
-  
+
+  private
+    def set_page
+      @page = params[:page] || 1
+    end
+
+    def index
+  properties = Property.page(@page).available
+  total_pages = properties.total_pages
+  render json: { properties: properties, total_pages: total_pages }
+end
+
 end
